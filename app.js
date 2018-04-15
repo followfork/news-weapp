f92f7f843bd7b61f2d0f0d806353f34a675484d2
followfork <xuwei_james@163.com>
2018-04-15 15:41:58 +0800

    movies

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ App({
   globalData: {
     g_isPlayingMusic: false,
     g_currentMusicPostId: null,
-    doubanBase: "https://api.douban.com",
+    // doubanBase: "https://api.douban.com",
+    doubanBase:'http://t.yushu.im',
     g_userInfo: null
   }
 })
